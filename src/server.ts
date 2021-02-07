5a384f5743b6f9fe9938eb75ab0a70575de36825
Keno Medenbach <keno@medenba.ch>
2021-02-07 12:33:10 +0100

    API endpoints moved to /api/...

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ export const server = opine()
 
 const api = new RiotApi(env.API_KEY, env.REGION)
 
-server.get('/live/:summonerName', async (req, res) => {
+server.get('/api/live/:summonerName', async (req, res) => {
   const summonerId = await api.getSummonerId(req.params.summonerName)
 
   const game = await api.getActiveGame(summonerId)
